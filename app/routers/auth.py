f0f9f7d297b11071d1d647aadc14c726cb355255
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-02-02 13:17:49 +0530

    Changed register endpoint to receive form_data instead of json (Need this change to include profile picture on register in future)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,15 @@
+from pydantic import EmailStr
 from app.dependencies import deps
 from app.crud.auth import UserCRUD
 from app.core import auth as auth_core
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import (
+    APIRouter, 
+    HTTPException, 
+    status, 
+    Depends,
+    Form
+)
 from app.schemas.auth import Token, User, UserRegister, UserResponse, TokenResponse
 
 # Arguments to pass to todo_router
@@ -17,7 +24,20 @@ auth_router = APIRouter(**AUTH_ROUTER_CONFIGURATION)
 token_handler = auth_core.TokenHandler()
 
 @auth_router.post('/register', status_code=status.HTTP_201_CREATED, response_model=UserResponse, tags=["Authentication"])
-def register_user(new_user: UserRegister):
+def register_user(
+    first_name: str = Form(..., max_length=30),
+    last_name: str = Form(..., max_length=30),
+    email: str = Form(..., max_length=255),
+    password: str = Form(..., min_length=8),
+    confirm_password: str = Form(..., min_length=8)
+    ):
+    new_user = UserRegister(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        confirm_password=confirm_password
+    )
     return UserCRUD().create(new_user)
 
 @auth_router.post('/login', status_code=status.HTTP_201_CREATED, response_model=TokenResponse, tags=["Authentication"])
